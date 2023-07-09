2f81da4425cb3c2f0d490671284a49d48ec267c6
Junaid Ahmed <junaidahmed501@gmail.com>
2023-07-09 20:04:03 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'creaition';
+  public title = 'creaition';
+
+  public isWorking = false;
+  public hasUploaded = false;
+
+  public urls = new Array<string>();
+  public outputImage = '';
+  public outputImagesSample = [
+    'assets/static/o1.png',
+    'assets/static/o2.png',
+    'assets/static/o3.png',
+    'assets/static/other1.png',
+    'assets/static/other2.png',
+    'assets/static/other3.png',
+  ];
+
+  detectFiles(event: any) {
+    this.urls = [];
+    let files = event.target.files;
+    if (files) {
+      for (let file of files) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          let imageURL = reader.result as string;
+          this.urls.push(imageURL);
+        }
+        reader.readAsDataURL(file)
+      }
+    }
+  }
+
+  handleUpload() {
+    this.isWorking = true;
+    this.hasUploaded = true;
+    this.isWorking = false;
+    this.outputImage = this.generateRandomImage();
+  }
+
+  handleReset() {
+    this.urls = [];
+    this.hasUploaded = false;
+  }
+
+  handleRegenerate() {
+    // todo: call this.handleUpload() again because we are not actually uploading anything for now
+    this.handleUpload();
+  }
+
+  generateRandomImage() {
+    const randomIndex = Math.floor(Math.random() * this.outputImagesSample.length);
+    return this.outputImagesSample[randomIndex];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NgOptimizedImage} from "@angular/common";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgOptimizedImage,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
